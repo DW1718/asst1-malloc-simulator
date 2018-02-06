4f92979b3daa8b7e7c98d070cbe51bf9a8ab1e0f
fredwlau <32381290+fredwlau@users.noreply.github.com>
2018-02-06 15:16:01 -0500

    Update mymalloc.c
    Added basic commenting

--- mymalloc.c
+++ mymalloc.c
@@ -27,6 +27,7 @@ void insertMem(theNode head, int size, theNode prev, theNode next){
 }
 
 void* mymalloc(int size, char* errLoc, int errLine){
+	//error checking for uninitialized memory or exceeding memory space
 	if(mainMem==NULL){
 		printf("Error.  Main memory uninitialized...\n");
 		return NULL;
@@ -41,6 +42,7 @@ void* mymalloc(int size, char* errLoc, int errLine){
 
 	int temp=5000;
 	while(1){
+		//checks if pointer is initialized
 		if(head->confirm){
 			if(temp>=(sizeof(node_t)+size)){
 				insertMem(tempHead, size, tempPrev, tempNext);
@@ -52,9 +54,12 @@ void* mymalloc(int size, char* errLoc, int errLine){
 			printf("Error.  No memory available...\n");
 			return NULL;
 		}
+		//checks if current memory is free and if it has enough space
 		tempPrev=tempHead->prev;
 		tempNext=tempHead->next;
 		if(tempHead->isFree==1 && tempHead->size>=size){
+			//checks previously allocated memory to see if it is larger then
+			//what the user asked for and creates an entry to manage that space
 			if(tempHead->size>size+sizeof(node_t)){
 				void *entry=(void*)tempHead+sizeof(node_t)+size;
 				theNode tempNode=(theNode) voidEntry;
@@ -87,21 +92,24 @@ void* mymalloc(int size, char* errLoc, int errLine){
 
 void myfree(void* ptr, char* errLoc, int errLine){
 	theNode trash = (theNode)(ptr-sizeof(node_t));
-
+	//valid pointer?
 	if(!((void*)mainMem<=(void*)trash&&(void*)trash<(void*)(mainMem+5000-sizeof(node_t)))){
 		printf("Error.  Invalid pointer...Out of Range...\n");
 		return;
 	}
+	//valid initialized pointer?
 	if(trash->confirm==0){
 		printf("Error.  Invalid pointer...\n");
 		return;
 	}
+	//already free?
 	if(trash->isFree){
 		printf("Error.  Pointer has already been freed...\n");
 		return;
 	}
-
+	//free
 	trash->isFree=1;
+	//adjusts nodes to check previous and next pointers
 	if(trash->next !=NULL&&trash->next->isFree){
 		trash->size = trash->size+trash->next->size+sizeof(node_t);
 		trash->next=trasn->next->next;
