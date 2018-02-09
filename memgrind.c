057f6f997e92585d04cd1faec8a8168a5263ef71
fredwlau <32381290+fredwlau@users.noreply.github.com>
2018-02-09 11:23:54 -0500

    Update memgrind.c
    Current working version, compiles and runs, need to add more functionality and error checking

--- memgrind.c
+++ memgrind.c
@@ -1,1 +1,18 @@
+/* memgrind.c source code to run our main and test our malloc and free functions*/
+/*Your memgrind.c should run all the workloads, one after the other, 100 times.  It
+should record the run time for each workload and store it.  When all 100 iterations
+of all the workloads have been run, memgrind.c should calculate the mean time for
+each workload to execute and output them in sequence.*/
+
 #include <stdio.h>
+#include <stdlib.h>
+#include <sys/time.h>
+#include <time.h>
+#include "mymalloc.h"
+
+
+int main(){
+    void* ptr=malloc(1);
+    printf("Address of malloc is %p\n", ptr);
+    return 0;
+}
