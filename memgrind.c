d9be38b7c25ca84dfde8a622ccae233fdf232d9d
Frederick Lau <fredwlau@scarletmail.rutgers.edu>
2018-02-15 20:35:50 -0500

    all

--- memgrind.c
+++ memgrind.c
@@ -31,27 +31,19 @@ int getrand(int min,int max){
 
 //malloc 1 byte and immediately free it, do this 150 times
 void testA(){
-	int testCount=0;
-	while(testCount<150){
-		printf("Test A number %d...................................................\n", testCount);
 		int i;
 		for(i=0;i<150;i++){
 			printf("Test A malloc\n");
 			void* ptr = malloc(1);
 			printf("Test A free\n");
 			free(ptr);
-		}
-		testCount++;
-	}
+		}	
 }
 
 //malloc 1 byte, store the pointer in an array, do this 150 times
 //Once you've malloced 150 byte chunks, free the 150 1 byte
 //pointers one by one!
 void testB(){
-	int testCount=0;
-	while(testCount<150){
-		printf("Test B number %d...................................................\n", testCount);
 		int i;
 		char* arr[150];
 		for(i=0;i<150;i++){
@@ -62,8 +54,6 @@ void testB(){
 			printf("Test B free\n");
 			free(arr[i]);
 		}
-		testCount++;
-	}
 }
 
 //Randomly choose between a 1 byte malloc() or free()ing a 1 byte pointer - do this 150 times
@@ -72,15 +62,12 @@ void testB(){
 //(don't allow a free() if you have no pointers to free())
 
 void testC(){
-	int testCount=0;
 	//RESOLVED(this is fine):
 	//if we run only 1 iteration we don't get any attempts to free a non existent address
 	//but once we run more then 1 iteration we start to see attempts to free non existent
 	//addresses, need to figure out why, I think it has something to do with the actual
 	//memory being recycled but i'm not sure how to verify or solve this if this is the case
 	//regardless, this testC function is the correct structure in which we need to run testC
-	while(testCount<150){
-		printf("Test C number %d...................................................\n", testCount);
 		int i=0;
 		int j=0;
 		float r;
@@ -90,28 +77,22 @@ void testC(){
 		for(m=0; m<150; m++){
 			arr[m] = NULL;
 		}
-		while(i<150&&j<150){
+
+		while(i<150){
 			r = (double)rand() / (double)RAND_MAX;
 			if (r<.5){
 				printf("Test C malloc...\n");
 				arr[i] = malloc(1);
-				if(arr[i]){
-					printf("Test C malloc is successful!\n");
-					i++;
-				}
+				i++;
 				continue;
 			}
 			else{
-				//printf("Test C free...\n");
 				int k;
 				//output says we are trying to free a NULL pointer, how?
 				for(k=0; k<150; k++){
-					//printf("%d\n", k);
 					if(arr[k]){
 						free(arr[k]);
 						arr[k]=NULL;
-						//printf("Test C free is successful!\n");
-						j++;
 						break;
 					}
 					else
@@ -119,8 +100,10 @@ void testC(){
 				}
 			}
 		}
-		testCount++;
-	}
+		//clear the remaining array
+		for(j=0; j<150; j++){
+			free(arr[j]);
+		}
 }
 /*Randomly choose between a randomly-sized malloc() or free()ing a pointer – do this many times (see below)
 
@@ -139,9 +122,6 @@ void testD(){
 	//that I am storing the pointers in should only be holding pointers that we
 	//initialized and assigned and I clear the locations of the array appropriately
 	//every time we free a pointer
-	int testCount=0;
-	while(testCount<1){
-		printf("Test D number %d...................................................\n", testCount);
 		int i=0;
 		int j=0;
 		int iter = 0;
@@ -184,8 +164,7 @@ void testD(){
 			iter++;
 		}
 		printf("Total memory allocation in D is %d\n", memAlloc);
-		testCount++;
-	}
+	
 }
 
 void testE(){
